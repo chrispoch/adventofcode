090b27a4fdc2e3868c5530da42d6d56b6f8ad2c1
Chris Poch <cpoch3@gatech.edu>
2020-12-11 19:16:59 -0500

    Final day 5

--- 5.py
+++ 5.py
@@ -30,12 +30,33 @@ with open("5.txt") as file:
 
     lines = file.readlines() 
     seats = []
+    rows = {}
     for line in lines:
         (row, col) = getSeat(line)
+        if row in rows:
+            rows[row] = rows[row] + 1
+        else:
+            rows[row] = 1
         seats.append(row * 8 + col)
         total = total + 1
 
     seats.sort(reverse=True)
     print(seats[0], total)
 
-#part 2
+    #part 2
+    myRow = []
+    for key in rows:
+        #print(key, str(rows[key]))
+        if rows[key] < 8:
+            myRow.append(key)
+    myRow.sort()
+    if len(myRow) > 1:
+        myRow = myRow[1]
+    else:
+        myRow = myRow[0]
+
+    for i in range(8):
+        seatId = myRow * 8 + i
+        if seatId not in seats:
+            print(seatId)
+            break
